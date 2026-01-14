37117df642f7a9e5d2729bc683658a1ce21c068c
Nazareno <lemongnz@gmail.com>
2026-01-13 22:25:46 -0300

    feat: implement ProductList grid layout

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import { Product } from '../models/Product';
+import { ProductItem } from './ProductItem';
+import './ProductList.css';
 
 interface ProductListProps {
   productList: Product[];
 }
 
-export const ProductList: React.FC<ProductListProps> = ({
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  productList,
-}) => {
-  return <div />;
+export const ProductList: React.FC<ProductListProps> = ({ productList }) => {
+  if (productList.length === 0) {
+    return (
+      <div className="product-list__empty">
+        <p>No se encontraron productos.</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="product-list">
+      {productList.map((product) => (
+        <ProductItem key={product.id} product={product} />
+      ))}
+    </div>
+  );
 };
