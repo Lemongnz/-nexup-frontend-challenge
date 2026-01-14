1fd8c3319d5070d184a6ec87309d7abd6329e80c
Nazareno <lemongnz@gmail.com>
2026-01-13 22:51:04 -0300

    feat: integrate listing and filtering logic

--- src/components/ProductManager.tsx
+++ src/components/ProductManager.tsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ProductList } from './ProductList';
 import { CategoryFilter } from './CategoryFilter';
+import { useProducts } from '../hooks/useProducts';
+import { CategoryFilterOption } from '../models/types';
 
 export const ProductManager: React.FC = () => {
+  const { products } = useProducts();
+  const [selectedCategory, setSelectedCategory] =
+    useState<CategoryFilterOption>('All');
+
+  const handleCategoryChange = (category: CategoryFilterOption) => {
+    setSelectedCategory(category);
+  };
+
+  const filteredProducts = products.filter((product) => {
+    if (selectedCategory === 'All') {
+      return true;
+    }
+    return product.category === selectedCategory;
+  });
+
   return (
     <div>
-      <CategoryFilter currentCategory="All" onCategoryChange={() => {}} />
-      <ProductList productList={[]} />
+      <CategoryFilter
+        currentCategory={selectedCategory}
+        onCategoryChange={handleCategoryChange}
+      />
+      <ProductList productList={filteredProducts} />
     </div>
   );
 };
